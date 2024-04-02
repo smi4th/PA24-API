4bafff637208671de2b8d05c6f8c3d95a7a52b41
smi4th <smiath.yt@gmail.com>
2024-04-03 00:28:18 +0200

    [ADD] : automation of generation for pytests files
        Based on the model.txt file, and the jsonFiles folder, the script will generate the pytests files.
    
        The pytests files use new fonctions from the tools.py file.

---
+++ pytest/init.py
@@ -0,0 +1,35 @@
+import os
+
+files = dict()
+
+for (dirpath, dirnames, filenames) in os.walk("pytest"):
+    for filename in filenames:
+        if '.json' in filename:
+            files[dirpath.replace("\\", "/").replace("pytest/jsonFiles/", "")] = files.get(dirpath.replace("\\", "/").replace("pytest/jsonFiles/", ""), []) + [filename]
+
+ROOT = "pytest/pyTests"
+
+# create the new files
+for key, value in files.items():
+
+    path = ''
+
+    if "delete" in key:
+        path = f"{ROOT.split("/")[0]}/pyTestsCleanup/test_{list(files).index(list(files)[-int(key.split('/')[0].split("_")[0])])}_{key.split("/")[0].split("_")[1]}_{value[0].split('_')[1]}.py"
+    else:
+        path = f"{ROOT}/{key.split('/')[0]}/test_{key.split('/')[0].split('_')[1]}_{key.split('/')[1]}.py"
+
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    
+    with open(path, "w") as f:
+        f.write("import tools\n\n")
+        with open("pytest/model.txt", "r") as m:
+            model = m.read()
+            value = sorted(value)
+
+            for v in value:
+                modelModified = model
+                modelModified = modelModified.replace("JSONFILE", f"test_{key.split('/')[0].split('_')[1]}_{'_'.join(v.split('_')[1::]).replace('.json', '')}")
+                modelModified = modelModified.replace("JSONPATH", f'"/pytest/jsonFiles/{key}/{v}"')
+                f.write(modelModified)
+                f.write("\n\n")
