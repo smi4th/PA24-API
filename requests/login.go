db1d87d90b480efde0992cfc0fe8361aba9159e3
smi4th <smiath.yt@gmail.com>
2024-05-02 23:32:39 +0200

    [ADD] : DELETE /login route to logout

--- requests/login.go
+++ requests/login.go
@@ -4,12 +4,17 @@ import (
 	"net/http"
 	"tools"
 	"database/sql"
+	"strings"
 )
 
 func Login(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	switch r.Method {
 	case "POST":
 		LoginPost(w, r, db)
+	case "GET":
+		LoginGet(w, r, db)
+	case "DELETE":
+		LoginDelete(w, r, db)
 	default:
 		tools.JsonResponse(w, 405, `{"message": "Method not allowed"}`)
 	}
@@ -80,5 +85,88 @@ func LoginPost(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 
 	// Sending the response
 	tools.JsonResponse(w, 201, jsonResponse)
+
+}
+
+func LoginGet(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	
+	// Getting the token from the header
+	token := r.Header.Get("Authorization")
+
+	// Checking if the token is empty
+	if tools.ValueIsEmpty(token) {
+		tools.JsonResponse(w, 400, `{"message": "Token cannot be empty"}`)
+		return
+	}
+
+	// Checking if the token exists in the database
+	result, err := tools.ExecuteQuery(db, "SELECT count(*) FROM `ACCOUNT` WHERE `token` = ?", strings.Replace(token, "Bearer ", "", 1))
+	if err != nil {
+		tools.ErrorLog(err.Error())
+		tools.JsonResponse(w, 500, `{"message": "Internal server error"}`)
+		return
+	}
+	defer result.Close()
+
+	// Checking if the token exists
+	var count int
+	result.Next()
+	result.Scan(&count)
+	if count == 0 {
+		tools.JsonResponse(w, 404, `{"message": "Token not found"}`)
+		return
+	}
+
+	// Creating the response
+	jsonResponse := `{"message": "Token found"}`
+	
+	// Sending the response
+	tools.JsonResponse(w, 200, jsonResponse)
+
+}
+
+func LoginDelete(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+
+	// Getting the token from the header
+	token := r.Header.Get("Authorization")
+
+	// Checking if the token is empty
+	if tools.ValueIsEmpty(token) {
+		tools.JsonResponse(w, 400, `{"message": "Token cannot be empty"}`)
+		return
+	}
+
+	// Checking if the token exists in the database
+	result, err := tools.ExecuteQuery(db, "SELECT count(*) FROM `ACCOUNT` WHERE `token` = ?", strings.Replace(token, "Bearer ", "", 1))
+	if err != nil {
+		tools.ErrorLog(err.Error())
+		tools.JsonResponse(w, 500, `{"message": "Internal server error"}`)
+		return
+	}
+	defer result.Close()
+
+	// Checking if the token exists
+	var count int
+	result.Next()
+	result.Scan(&count)
+	if count == 0 {
+		tools.JsonResponse(w, 404, `{"message": "Token not found"}`)
+		return
+	}
+
+	// Inserting the Housing in the database
+	result, err = tools.ExecuteQuery(db, "UPDATE account SET token = NULL WHERE token = ?", strings.Replace(token, "Bearer ", "", 1))
+	if err != nil {
+		tools.ErrorLog(err.Error())
+		tools.JsonResponse(w, 500, `{"message": "Internal server error"}`)
+		return
+	}
+	defer result.Close()
+
+	// Creating the response
+	jsonResponse := `{"message": "Token deleted"}`
+
+	// Sending the response
+	tools.JsonResponse(w, 200, jsonResponse)
 
 }
